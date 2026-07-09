06d449221646354bc2c446bc715bb7cd3670cc53
Princeton Afeez <princetonafeez@gmail.com>
2026-07-09 13:30:04 -0700

    Data migration assigning existing rows to a default tenant

--- shop/migrations/0010_backfill_default_tenant.py
+++ shop/migrations/0010_backfill_default_tenant.py
@@ -1,3 +1,4 @@
+# Data migration assigning existing rows to a default tenant
 from django.db import migrations
 
 # Tenant-scoped models that need their existing rows attached to the default tenant.
