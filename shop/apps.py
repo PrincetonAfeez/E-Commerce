1b7daea51653b93c132f57baa09f98869c11cb8e
Princeton Afeez <princetonafeez@gmail.com>
2026-07-20 09:31:06 -0700

    Django AppConfig registering the shop application

--- shop/apps.py
+++ shop/apps.py
@@ -1,7 +1,20 @@
-# Django AppConfig registering the shop application
+"""Django AppConfig registering the shop application"""
 from django.apps import AppConfig
 
 
 class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "shop"
+
+    def ready(self) -> None:
+        from django.conf import settings
+        from django.db.utils import OperationalError
+
+        if getattr(settings, "RUNNING_TESTS", False):
+            return
+        try:
+            from shop.csrf_origins import extend_csrf_trusted_origins
+
+            extend_csrf_trusted_origins()
+        except OperationalError:
+            pass
